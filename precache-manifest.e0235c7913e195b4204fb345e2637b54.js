fc929a24df69332459408e54a155ad25d9c58c57
Igor <igor@pop-os.localdomain>
2019-07-14 12:21:16 +0200

    Updates

---
+++ precache-manifest.e0235c7913e195b4204fb345e2637b54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aea3ea71c9182a2bec2fe02318d7a2c",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "44e316051479cc98443b",
+    "url": "/contact-manager/static/css/2.df9ef6f4.chunk.css"
+  },
+  {
+    "revision": "2c101bf04a090d7dde13",
+    "url": "/contact-manager/static/css/main.4c02f4a6.chunk.css"
+  },
+  {
+    "revision": "44e316051479cc98443b",
+    "url": "/contact-manager/static/js/2.523ad079.chunk.js"
+  },
+  {
+    "revision": "2c101bf04a090d7dde13",
+    "url": "/contact-manager/static/js/main.96c5ecc7.chunk.js"
+  },
+  {
+    "revision": "a25dc548b2853493d42e",
+    "url": "/contact-manager/static/js/runtime~main.b61356a3.js"
+  }
+]);
